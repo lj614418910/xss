b9c899c990ba1d41af35a87691a20ec5a29fa4e1
lijia_i <lijia_i@didichuxing.com>
2017-04-28 19:51:52 +0800

    通过url来进行xss攻击

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.set('X-XSS-Protection',0);
+  res.render('index', { title: 'Express' ,xss:req.query.xss});
 });
 
 module.exports = router;
